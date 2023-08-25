1bb48b248b05e4289fd2c5f4dac104e17acceb96
Denilson <denilsonrockmetal@hotmail.com>
2023-08-25 15:45:04 -0300

    Project ms-hr-eureka-server created

---
+++ ms-hr-eureka-server/src/main/java/com/ms/eurekaserver/MsHrEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.ms.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrEurekaServerApplication.class, args);
+	}
+
+}
