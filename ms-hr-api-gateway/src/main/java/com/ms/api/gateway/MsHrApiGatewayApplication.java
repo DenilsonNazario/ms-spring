643602c6b6dd5d4e77328c122f87041706c1cede
Denilson <denilsonrockmetal@hotmail.com>
2023-08-26 19:00:04 -0300

    Project hr-api-gateway-zuul created

---
+++ ms-hr-api-gateway/src/main/java/com/ms/api/gateway/MsHrApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.ms.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrApiGatewayApplication.class, args);
+	}
+
+}
