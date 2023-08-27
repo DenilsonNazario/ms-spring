d4c39d8dc67c513dd369e0ced93ee806b5a34e98
Denilson <denilsonrockmetal@hotmail.com>
2023-08-27 16:56:04 -0300

    Project hr-config-server created

---
+++ ms-hr-config-server/src/main/java/com/ms/config/server/MsHrConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.ms.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrConfigServerApplication.class, args);
+	}
+
+}
