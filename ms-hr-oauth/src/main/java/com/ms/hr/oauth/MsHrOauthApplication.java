f70051925107979253268bf37b3836ff8a79486b
Denilson <denilsonrockmetal@hotmail.com>
2023-08-31 12:51:54 -0300

    Project hr-oauth created

---
+++ ms-hr-oauth/src/main/java/com/ms/hr/oauth/MsHrOauthApplication.java
@@ -0,0 +1,13 @@
+package com.ms.hr.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsHrOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsHrOauthApplication.class, args);
+	}
+
+}
