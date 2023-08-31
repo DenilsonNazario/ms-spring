cba1f339be1c2ca00c9c12f5188d389ed8953bec
Denilson <denilsonrockmetal@hotmail.com>
2023-08-31 02:03:49 -0300

    Database seeding

--- ms-hr-user/src/main/java/com/ms/hr/user/MsHrUserApplication.java
+++ ms-hr-user/src/main/java/com/ms/hr/user/MsHrUserApplication.java
@@ -1,15 +1,26 @@
 package com.ms.hr.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class MsHrUserApplication {
+public class MsHrUserApplication implements CommandLineRunner {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsHrUserApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("BCRYPT = " + this.passwordEncoder.encode("1234567"));
 	}
 
 }
